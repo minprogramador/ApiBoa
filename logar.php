cd2a6f45c27e05a17347c3fb9a9d190ff2b3416a
minprogramador <minprogramador@gmail.com>
2019-01-20 19:26:36 -0200

    separacao logar com logar em cpf

--- logar.php
+++ logar.php
@@ -1,5 +1,10 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 use Api\Boa\Logar;
 use Api\Boa\Check;
 use Api\Boa\Consultar;
@@ -25,17 +30,53 @@ if(count($argv) > 1) {
 			$senha = str_replace('s=', '', $arv);
 		}elseif(stristr($arv, 'p=')){
 			$proxy = str_replace('p=', '', $arv);
+		}elseif(stristr($arv, 't=')){
+			$tipo = str_replace('t=', '', $arv);
 		}
 	}
 }else{
 	//die('falta parametros...');
 }
 
+if(!isset($tipo)) {
+	$tipo = 'cpf';
+}
 
 if($usuario != null && $senha != null && $proxy != null){
+
 	$logar->setProxy($proxy);
 	$logar->setUsuario($usuario);
 	$logar->setSenha($senha);
+
+	$prlogin = $logar->preLogin();
+
+	if(is_array($prlogin)) {
+		
+		$cookie  = $prlogin['cookie'];
+		$ecs     = $prlogin['ecs'];
+		$encript = $prlogin['encript'];
+
+		if($tipo == 'cpf') {
+			$cookie = $logar->runCpf($cookie, $ecs, $encript);
+		}elseif($tipo == 'cnpj') {
+
+
+
+		}else{
+			echo 'nenhum tipo informado =/';
+			die;
+		}
+
+		echo $cookie;
+
+
+	}else{
+		echo "nao tem array\n";
+		print_r($prlogin);
+	}
+	echo "\n";
+	die;
+
 	$cookie = $logar->run();
 
 	if($cookie == 'rede'){
